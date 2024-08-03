96f7d3b69a46ce47afc16e3a491defb2e328a9bc
KatalinLengyel <158131186+KatalinLengyel@users.noreply.github.com>
2024-08-03 17:33:51 +0300

    Functia de search cu Header

---
+++ src/js/discoveryapi.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const API_KEY = 'JFfDy34LrJl7VVgBTc7jqnnTRHXCQbbZ';
+const URL = 'https://app.ticketmaster.com/discovery/v2/events';
+
+const HITSPERPAGE = 16;
+
+const fetchEvents = async (query = "", country = undefined, page = 1) => {
+  try {
+    const response = await axios.get(`${URL}`, {
+      params: {
+        apikey: API_KEY,
+        keyword: query,
+        page,
+        per_page: `${HITSPERPAGE}`,
+        countryCode: country,
+      },
+    });
+    // console.log(typeof response);
+    return response.data;
+  } catch (error) {
+    console.log('Error:', error);
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  }
+};
+
+export {fetchEvents};
