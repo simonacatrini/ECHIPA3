62ef4f8a48ea2710f7bafd39a336ad52ae9150d4
Corina Ionelia <corynaionelia@gmail.com>
2024-08-06 21:24:10 +0300

    corina

--- src/js/searchbar.js
+++ src/js/searchbar.js
@@ -5,24 +5,23 @@ import { generatePagination } from './pagination';
 import Notiflix from 'notiflix';
 import pin from "../images/vector.svg"
 
-// const form = document.querySelector('.search-form');
 const itemGallery = document.querySelector('.item-gallery');
 const dropdownList = document.querySelector('.dropdown-list');
 const pagesList = document.querySelector(".pages");
 
 const populateCountriesDropdown = () => {
-  const markup = countries
-    .map(country => {
-      return `<option value="${country.code}">${country.name}</option>`;
-    })
-    .join('');
-  dropdownList.innerHTML = markup;
+    const markup = countries
+        .map(country => {
+            return `<option value="${country.code}">${country.name}</option>`;
+        })
+        .join('');
+    dropdownList.innerHTML = markup;
 };
 
 const populateEventGallery = events => {
-  const markup = events
-    .map(
-      event => `<div class="item-card">
+    const markup = events
+        .map(
+            (event, index) => `<div class="item-card" style="animation-delay: ${index * 0.1}s;">
         <a href="${event.images[0].url}" target="_blank">
             <div class="image-wrapper">
                 <img src="${event.images[0].url}" alt="${event.name}" loading="lazy" width="267"/>
@@ -41,39 +40,37 @@ const populateEventGallery = events => {
             </p>
         </div>
     </div>`
-    )
-    .join('');
-  itemGallery.innerHTML = markup;
-  
-  
+        )
+        .join('');
+    itemGallery.innerHTML = markup;
 };
 
-const processEventData = async () => {
+const processEventData = async() => {
     const query = localStorage.getItem("query") || "";
     const country = localStorage.getItem("country") || "";
     const page = localStorage.getItem("page") || 1;
 
-  const eventsObject = await fetchEvents(query, country, page);
-  
-  if (
-    eventsObject &&
-    eventsObject.data &&
-    eventsObject.data._embedded &&
-    eventsObject.data._embedded.events.length > 0
-  ) {
-    populateEventGallery(eventsObject.data._embedded.events);
-    generatePagination(page, eventsObject.totalCount);
-  } else {
-    itemGallery.innerHTML = '';
-    Notiflix.Notify.failure('Sorry, there are no results.');
-  }
+    const eventsObject = await fetchEvents(query, country, page);
+
+    if (
+        eventsObject &&
+        eventsObject.data &&
+        eventsObject.data._embedded &&
+        eventsObject.data._embedded.events.length > 0
+    ) {
+        populateEventGallery(eventsObject.data._embedded.events);
+        generatePagination(page, eventsObject.totalCount);
+    } else {
+        itemGallery.innerHTML = '';
+        Notiflix.Notify.failure('Sorry, there are no results.');
+    }
 };
 
-const onCountryChange = async () => {
-  const selectedCountry = dropdownList.value;
-  localStorage.setItem("country", selectedCountry);
-  localStorage.setItem("page", 1);
-  await processEventData();
+const onCountryChange = async() => {
+    const selectedCountry = dropdownList.value;
+    localStorage.setItem("country", selectedCountry);
+    localStorage.setItem("page", 1);
+    await processEventData();
 };
 
 const onPageChange = (e) => {
@@ -82,12 +79,16 @@ const onPageChange = (e) => {
     if (isNaN(selectedValue)) {
         return;
     }
+    const currentPageElement = document.querySelector('.page.active');
+    if (currentPageElement) {
+        currentPageElement.classList.remove('active');
+    }
+    e.target.classList.add('active');
     localStorage.setItem("page", selectedValue);
     processEventData();
 }
 
-// form.addEventListener('search', onSearch);
-pagesList.addEventListener('click', onPageChange)
+pagesList.addEventListener('click', onPageChange);
 dropdownList.addEventListener('change', onCountryChange);
 
-export {populateCountriesDropdown, populateEventGallery, processEventData, onCountryChange};
+export { populateCountriesDropdown, populateEventGallery, processEventData, onCountryChange };
